7c327d5aff1f386d51639f8dd768b50d8b8fb198
mxqt <maxquinntapara@gmail.com>
2021-02-09 13:45:51 +1300

    dd

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-$('#addy').on('touchstart', function() {});
+$(document).ready(function() {
+    $('.hover').on('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
+});
