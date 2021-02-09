a7065d5e07154a74b928b80c14e90c072ca1c135
mxqt <maxquinntapara@gmail.com>
2021-02-09 14:02:59 +1300

    ddf

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
 $(document).ready(function() {
-    $('#addy' + '#roro' + '#pu' + '#rua' + '#con' + '#te').on('touchstart touchend', function(e) {
+    $('#addy').on('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+        console.log('hallo')
+    });
+});
+
+
+$(document).ready(function() {
+    $('#te').on('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+        console.log('hallo')
+    });
+});
+
+$(document).ready(function() {
+    $('#roro').on('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+        console.log('hallo')
+    });
+});
+
+$(document).ready(function() {
+    $('#pu').on('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+        console.log('hallo')
+    });
+});
+
+$(document).ready(function() {
+    $('#con').on('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+        console.log('hallo')
+    });
+});
+
+$(document).ready(function() {
+    $('#rua').on('touchstart touchend', function(e) {
         e.preventDefault();
         $(this).toggleClass('hover_effect');
         console.log('hallo')
