175c12f6088c13c9d1ecb811824c923d43f7c241
mxqt <maxquinntapara@gmail.com>
2021-02-09 13:58:11 +1300

    e

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#addy', '#roro', '#pu', '#rua', '#con', '#te').on('touchstart touchend', function(e) {
+    $('#addy' + '#roro' + '#pu' + '#rua' + '#con' + '#te').on('touchstart touchend', function(e) {
         e.preventDefault();
         $(this).toggleClass('hover_effect');
         console.log('hallo')
